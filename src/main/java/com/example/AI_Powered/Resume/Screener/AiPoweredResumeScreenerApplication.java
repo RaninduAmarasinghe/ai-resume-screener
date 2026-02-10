de445208b28ca0e6f5c63b834379a0e6c5a19db2
Ranindu Amarasinghe <raninduamarasinghe@gmail.com>
2026-02-10 19:13:58 +0530

    first commit

---
+++ src/main/java/com/example/AI_Powered/Resume/Screener/AiPoweredResumeScreenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.AI_Powered.Resume.Screener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPoweredResumeScreenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPoweredResumeScreenerApplication.class, args);
+	}
+
+}
